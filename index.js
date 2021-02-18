367c33c6d52932a476e0dff51d0ab637bf7328e2
NamanGoyal07 <naman18@iitg.ac.in>
2021-02-18 13:18:22 +0530

    Cf scrap

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const request = require('request');
 const cheerio = require('cheerio');
 
-request('https://javascript.info/async-await', (error, response, html) =>{
+request('https://codeforces.com/blog/entry/87934', (error, response, html) =>{
     if(!error && response.statusCode === 200){
         const $ = cheerio.load(html);
 
         const content = $('.content');
-        console.log(content.html());
+        // console.log(content.html());
+        // console.log(content.text());
+
+        const output = content.find('h1').text();
+        console.log(output);
 
     }
 });
